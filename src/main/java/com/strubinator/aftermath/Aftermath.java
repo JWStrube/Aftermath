4fa50731f02ad2aa339e48c391d0ae9f134a12f6
John Strube <jwstrube@gmail.com>
2015-02-26 21:59:04 -0800

    Added Config and Logging handlers Added Config GUI

--- src/main/java/com/strubinator/aftermath/Aftermath.java
+++ src/main/java/com/strubinator/aftermath/Aftermath.java
@@ -1,8 +1,10 @@
 package com.strubinator.aftermath;
 
 
+import com.strubinator.aftermath.handler.ConfigurationHandler;
 import com.strubinator.aftermath.proxy.IProxy;
 import com.strubinator.aftermath.reference.Reference;
+import com.strubinator.aftermath.utility.LogHelper;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -11,30 +13,31 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class Aftermath
 {
     @Instance(Reference.MOD_ID)
     public static Aftermath instance;
 
-    @SidedProxy(clientSide = "com.strubinator.aftermath.proxy.ClientProxy", serverSide = "com.strubinator.aftermath.proxy.ServerProxy")
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static IProxy proxy;
     
     @EventHandler
     public void PreInit(FMLPreInitializationEvent event)
     {
-		
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        LogHelper.info("Pre Initialization Complete");
     }
 
     @EventHandler
     public void Init(FMLInitializationEvent event)
     {
-
+        LogHelper.info("Initialization Complete");
     }
 
     @EventHandler
     public void PostInit(FMLPostInitializationEvent event)
     {
-		
+        LogHelper.info("Post Initialization Complete");
     }
 }
