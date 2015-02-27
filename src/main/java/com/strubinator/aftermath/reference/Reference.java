180eb4d840dccb225d76ef4842c1991fba478162
John Strube <jwstrube@gmail.com>
2015-02-26 21:57:24 -0800

    Added references

--- src/main/java/com/strubinator/aftermath/reference/Reference.java
+++ src/main/java/com/strubinator/aftermath/reference/Reference.java
@@ -10,7 +10,7 @@ public class Reference
     public static final String VERSION = "1.0";
     public static final String CLIENT_PROXY_CLASS = "com.strubinator.aftermath.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.strubinator.aftermath.proxy.ServerProxy";
-
+    public static final String GUI_FACTORY_CLASS = "com.strubinator.aftermath.client.gui.GuiFactory";
 
 
 
