8ebe7467483340a693267d091da5b6929bc7a0ac
John Strube <jwstrube@gmail.com>
2015-02-26 20:22:12 -0800

    Added references to Proxy class locations

--- src/main/java/com/strubinator/aftermath/reference/Reference.java
+++ src/main/java/com/strubinator/aftermath/reference/Reference.java
@@ -8,6 +8,8 @@ public class Reference
     public static final String MOD_ID = "aftermath";
     public static final String MOD_NAME = "Aftermath";
     public static final String VERSION = "1.0";
+    public static final String CLIENT_PROXY_CLASS = "com.strubinator.aftermath.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.strubinator.aftermath.proxy.ServerProxy";
 
 
 
