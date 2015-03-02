2d7c985ed3c013fbbcc454d948b464b4ec0c2fce
John Strube <jwstrube@gmail.com>
2015-03-01 20:44:32 -0800

    Changed BlockLeadOre to generic BlockOre class

--- src/main/java/com/strubinator/aftermath/init/ModBlocks.java
+++ src/main/java/com/strubinator/aftermath/init/ModBlocks.java
@@ -2,7 +2,7 @@ package com.strubinator.aftermath.init;
 
 import com.strubinator.aftermath.block.BlockAftermath;
 import com.strubinator.aftermath.block.BlockLeadBlock;
-import com.strubinator.aftermath.block.BlockLeadOre;
+import com.strubinator.aftermath.block.BlockOre;
 import com.strubinator.aftermath.reference.Names;
 import com.strubinator.aftermath.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -13,12 +13,12 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks
 {
-    public static final BlockAftermath leadOre = new BlockLeadOre();
+    public static final BlockAftermath leadOre = new BlockOre();
     public static final BlockAftermath leadBlock = new BlockLeadBlock();
 
     public static void init()
     {
-        GameRegistry.registerBlock(leadOre, Names.Blocks.LEAD_ORE);
+        GameRegistry.registerBlock(leadOre, Names.Blocks.LEAD_ORE).setBlockName(Names.Blocks.LEAD_ORE).setBlockTextureName(Names.Blocks.LEAD_ORE).setHardness(3.0F).setResistance(5.0F);
         GameRegistry.registerBlock(leadBlock, Names.Blocks.LEAD_BLOCK);
     }
 }
