bd6779abb1dbb9b646b4dbace61c4f0c881f9628
John Strube <jwstrube@gmail.com>
2015-03-01 20:25:07 -0800

    Changed BlockLeadOre to generic BlockOre class

---
+++ src/main/java/com/strubinator/aftermath/block/BlockOre.java
@@ -0,0 +1,92 @@
+package com.strubinator.aftermath.block;
+
+import com.strubinator.aftermath.creativetab.CreativeTabAftermath;
+import com.strubinator.aftermath.item.ItemAftermath;
+import com.strubinator.aftermath.reference.Names;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+/**
+ * Created by John on 2/28/2015.
+ */
+public class BlockOre extends BlockAftermath
+{
+    public BlockOre()
+    {
+        super();
+
+    }
+
+    //Returns item that drops from block. Used for ores that drop items
+    public Item getItemDropped(int i1, Random j1, int k1)
+    {
+        return Item.getItemFromBlock(this);
+    }
+
+    /**
+     * For ores that drop random amount of items
+     */
+    public int quantityDropped(Random i1)
+    {
+        return 1;
+    }
+
+    /**
+     * Returns the usual quantity dropped by the block plus a bonus of 1 to 'i' (inclusive).
+     */
+    public int quantityDroppedWithBonus(int i1, Random j1)
+    {
+        if (i1 > 0 && Item.getItemFromBlock(this) != this.getItemDropped(0, j1, i1))
+        {
+            int j = j1.nextInt(i1 + 2) - 1;
+
+            if (j < 0)
+            {
+                j = 0;
+            }
+
+            return this.quantityDropped(j1) * (j + 1);
+        }
+        else
+        {
+            return this.quantityDropped(j1);
+        }
+    }
+
+    /**
+     * Drops the block items with a specified chance of dropping the specified items
+     */
+    public void dropBlockAsItemWithChance(World i1, int j1, int k1, int l1, int m1, float n1, int o1)
+    {
+        super.dropBlockAsItemWithChance(i1, j1, k1, l1, m1, n1, o1);
+    }
+
+    private Random rand = new Random();
+    @Override
+    public int getExpDrop(IBlockAccess i1, int j1, int k1)
+    {
+        if (this.getItemDropped(j1, rand, k1) != Item.getItemFromBlock(this))
+        {
+            int i2 = 0;
+
+            //Add exp drop for ores that do not drop themselves
+
+            return i2;
+        }
+        return 0;
+    }
+
+    /**
+     * Determines the damage on the item the block drops. Used in cloth and wood.
+     */
+    public int damageDropped(int i1)
+    {
+        return  0;
+    }
+}
